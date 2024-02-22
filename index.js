554c6503d163ed91109008486b934bf4a46298c7
Brendan Murphy <brendan.murphy@ostusa.com>
2024-02-22 09:47:44 -0500

    Can create

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.text({ type: 'text/html' }));
+
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('Databse Connected Successfully!!');
+  })
+  .catch((err) => {
+    console.log('Could not connect to the database', err);
+    process.exit();
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const UserRoute = require('./routes/User');
+app.use('/user', UserRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
